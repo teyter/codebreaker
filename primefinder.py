e296492a609500410cf0b230c0baf8ffea51a2d2
teyter <teitur93@gmail.com>
2025-02-20 12:59:17 +0000

    optimized with odds, sqrt

--- primefinder.py
+++ primefinder.py
@@ -1,14 +1,19 @@
+from math import sqrt
+
 lst = [2,3,5]
 
-n = 6
+n = 7
 while len(lst) < 10001:
     b = True
     for i in lst:
+        sqr = int(sqrt(n)) + 1
+        if i > sqr:
+            break
         if n % i == 0:
             b = False
             break
     if b:
         lst.append(n)
-    n += 1
+    n += 2
 
 print(lst[-1])
